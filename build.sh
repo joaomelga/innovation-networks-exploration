1953d29604647f06a46ae5b3e5d381866dbc9092
João Melga <joao.melga@analogway.com>
2025-08-25 21:13:26 +0200

    chore: update thesis title and related documents for consistency

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Copy buildmain.pdf to the target filename
-cp ./reports/texts/build/main.pdf "Melga - 2025 - Nested Syndication Networks.pdf"
+cp ./reports/texts/build/main.pdf "Melga - 2025 - Nested Investor Syndication Networks.pdf"
